d00fd20d0fec0e5a5c07b009047277b20594ca60
suiramus <suiramus@gmail.com>
2024-12-10 15:22:51 +0200

    ...

--- swiper-photoswipe/js/script.js
+++ swiper-photoswipe/js/script.js
@@ -3,18 +3,33 @@ import PhotoSwipeLightbox from './photoswipe-lightbox.esm.min.js';
 import PhotoSwipe from './photoswipe.esm.min.js';
 
 	// Initialize Swiper
-	const swiper = new Swiper('.swiper-container', {
+	const swiper1 = new Swiper('.swiper-1', {
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
 		},
 		slidesPerView: 1,
 		spaceBetween: 10,
-		loop: true,
+		// loop: true,
 	});
 	
 	// Initialize Swiper 2 without Photoswipe
 	const swiper2 = new Swiper('.swiper-2', {
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		slidesPerView: 1,
+		spaceBetween: 10,
+		// loop: true,
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+	});
+	
+	// Initialize Swiper 3
+	const swiper3 = new Swiper('.swiper-3', {
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
@@ -26,14 +41,22 @@ import PhotoSwipe from './photoswipe.esm.min.js';
 			el: ".swiper-pagination",
 			clickable: true,
 		},
-		loop: false,
 	});
 
 	// Initialize Photoswipe
-	const lightbox = new PhotoSwipeLightbox({
-		gallery: '.swiper-container, .gallery',
+	const lightbox1 = new PhotoSwipeLightbox({
+		gallery: '.swiper-1, .gallery',
 		children: 'a',
 		pswpModule: PhotoSwipe,
 	});
-
-	lightbox.init();
+	lightbox1.init();
+	
+	
+	// Initialize Photoswipe for single image
+	const lightbox2 = new PhotoSwipeLightbox({
+		gallery: '.lb-single',
+		// children: '', // https://photoswipe.com/options/#lightbox-module-options
+		children: false,
+		pswpModule: PhotoSwipe,
+	});
+	lightbox2.init();
